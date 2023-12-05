82154ab9371ff0163539593335a6039036f52bad
phudyka <paul.hudyka@gmail.com>
2023-12-05 15:28:39 +0100

    makefile

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:24:46 by phudyka           #+#    #+#             */
-/*   Updated: 2023/11/14 15:52:49 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/12/05 15:08:33 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 
 int main(void)
 {
-    // ClapTrap clap(ORANGE "CL4P" RESET);
-    // std::cout << std::endl;
-    // std::cout << clap << std::endl;
-    // clap.attack(RED "target" RESET);
-    // clap.takeDamage(3);
-    // clap.beRepaired(2);
-    // std::cout << clap << std::endl;
+    ClapTrap clap(ORANGE "CL4P" RESET);
+    std::cout << std::endl;
+    std::cout << clap << std::endl;
+    clap.attack(RED "target" RESET);
+    clap.takeDamage(3);
+    clap.beRepaired(2);
+    std::cout << clap << std::endl;
 
-    // ScavTrap scav(BLUE "5CAV" RESET);
-    // std::cout << scav << std::endl;
-    // scav.guardGate();
-    // std::cout << std::endl;
+    ScavTrap scav(BLUE "5CAV" RESET);
+    std::cout << scav << std::endl;
+    scav.guardGate();
+    std::cout << std::endl;
 
     FragTrap frag(YELLOW "K-B00M" RESET);
     std::cout << frag << std::endl;
